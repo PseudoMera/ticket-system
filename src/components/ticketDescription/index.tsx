d658e744c7308be1ea5a1995827afd58c11544f6
Bant89 <bant8009@gmail.com>
2020-10-16 10:45:08 -0400

    added formik to ticket description

--- src/components/ticketDescription/index.tsx
+++ src/components/ticketDescription/index.tsx
@@ -1,30 +1,58 @@
 import React, { memo, useState } from "react"
-import { Dropdown } from 'primereact/dropdown';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+import { Formik, Form, Field, ErrorMessage } from "formik"
+import * as Yup from "yup"
+import { Dropdown } from 'primereact/dropdown'
 import { TICKET_STATUS } from '../../constants'
 import "./style.scss"
 
+const TicketSchema = Yup.object().shape({
+    title: Yup.string().min(10, 'Muy corto').required('Requerido'),
+    description: Yup.string().min(10, 'Muy corto').required('Requerido'),
+    status: Yup.mixed().oneOf(Object.keys(TICKET_STATUS))
+})
+
+type DropItems = {
+    status: string;
+    value: string;
+}
+
 const TicketDescription: React.FC = () => {
-    const [status, setStatus] = useState("")
-    const [description, setDescription] = useState("")
+    const [dropDownItems, _] = useState<Array<DropItems>>(() => {
+        let initialValue = []
+        for (const [key, value] of Object.entries(TICKET_STATUS)) {
+            let result = value[0] + value.substring(1).toLocaleLowerCase().replace('_', ' ')
+            initialValue.push({ status: result, value: key })
+        }
+        return initialValue
+    })
     return (
-        <Container>
-            <Row sm={12}>
-                <h2>Nombre usuario</h2>
-            </Row>
-            <Row sm={12}>
-                <div className="dropdownSelector">
-                    <h3>Estatus</h3>
-                    <Dropdown value={status} options={TICKET_STATUS} onChange={(e) => { setStatus(e.value) }} placeholder="Select a Status" />
-                </div>
-            </Row>
-            <Row sm={12}>
-                <input className="descriptionInput" type="text" value={description} onChange={(e) => {
-                    setDescription(e.target.value)
-                }} />
-            </Row>
-        </Container>
+        <div>
+            <h1>Ticket Description</h1>
+            <Formik
+                initialValues={{
+                    title: '',
+                    description: '',
+                    status: TICKET_STATUS.OPEN
+                }}
+                validationSchema={TicketSchema}
+                onSubmit={console.log}
+            >
+                {({ values, setFieldValue }) => (
+                    <Form>
+                        <Field name="title" id="title" placeholder="Titulo del ticket" className="inputElement" />
+                        <ErrorMessage name="title" />
+                        <Field name="description" id="description" placeholder="Descripcion del ticket" as="textarea" className="inputElement" />
+                        <ErrorMessage name="title" />
+                        <section className="selectSection">
+                            <h3>Estado: </h3>
+                            <Dropdown optionLabel="status" value={values.status} options={dropDownItems} onChange={e => setFieldValue("status", e.value)} placeholder="Estado del ticket" style={{ width: "180px" }} />
+                        </section>
+                        <ErrorMessage name="status" />
+                        <button type="submit">Crear ticket</button>
+                    </Form>
+                )}
+            </Formik>
+        </div>
     )
 }
 
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,7 @@
 export const PROJECT_NAME = 'ticket-system';
 
-export const TICKET_STATUS = [{
-    label: 'Open', value: 'OPEN'
-},
-{ label: 'Closed', value: 'CLOSED' },
-{
-    label: 'In progress', value: 'IN_PROGRESS'
-}]
+export const TICKET_STATUS = {
+    OPEN: 'OPEN',
+    IN_PROGRESS: 'IN_PROGRESS',
+    CLOSED: 'CLOSED'
+}
